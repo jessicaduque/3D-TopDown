4ff586b4cfdc7530dcf4e6e261a5826852581171
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2023-04-17 19:40:06 -0300

    Mais base

--- Assets/Scripts/Heroi.cs
+++ Assets/Scripts/Heroi.cs
@@ -9,9 +9,15 @@ public class Heroi : MonoBehaviour
     private Vector3 Destino;
     public GameObject MeuAtaque;
     private Animator ControlAnim;
+    private GerenciadorDeObjetos Inventario;
+    public float hp = 50;
+
+    private bool podePegar = false;
+
 
     void Start()
     {
+        Inventario = GameObject.FindGameObjectWithTag("Inventario").GetComponent<GerenciadorDeObjetos>();
         Destino = new Vector3(0, 0, 0);
         Agente = GetComponent<NavMeshAgent>();
         ControlAnim = GetComponent<Animator>();
@@ -26,15 +32,29 @@ public class Heroi : MonoBehaviour
             RaycastHit localTocou;
             if (Physics.Raycast(pontodesaida, out localTocou, Mathf.Infinity))
             {
-                if(localTocou.collider.gameObject.tag == "Inimigo")
+                if (localTocou.collider.gameObject.tag == "Inimigo")
                 {
+                    Agente.stoppingDistance = 2;
                     Destino = localTocou.transform.position;
+
                 }
                 else
                 {
+                    Agente.stoppingDistance = 0;
                     Destino = localTocou.point;
                 }
+
             }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Destino = transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ControlAnim.SetTrigger("Pegar");
         }
         Agente.SetDestination(Destino);
 
@@ -48,10 +68,16 @@ public class Heroi : MonoBehaviour
         {
             ControlAnim.SetTrigger("Ataque");
         }
+
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             ControlAnim.SetTrigger("Disparo");
         }
+
+
+
+
+
     }
     public void AtivarAtk()
     {
@@ -63,16 +89,52 @@ public class Heroi : MonoBehaviour
         MeuAtaque.SetActive(false);
     }
 
+    public void AtivarPegada()
+    {
+        podePegar = true;
+    }
+
+    public void DesativarPegada()
+    {
+        podePegar = false;
+    }
+
+
     void AtkDistancia()
     {
         RaycastHit meuAtkD;
         if (Physics.Raycast(MeuAtaque.transform.position, transform.forward, out meuAtkD, 10f))
         {
-            if(meuAtkD.collider.gameObject.tag == "Inimigo")
+
+            if (meuAtkD.collider.gameObject.tag == "Inimigo")
             {
-               meuAtkD.collider.gameObject.GetComponent<Inimigo>().TomeiDano();
+                meuAtkD.collider.gameObject.GetComponent<Inimigo>().TomeiDano();
             }
+
         }
+    }
+
+
+    private void OnTriggerStay(Collider colidiu)
+    {
+        if (colidiu.gameObject.tag == "Pegavel")
+        {
+            if (podePegar == true)
+            {
+                string nomeObj = colidiu.gameObject.GetComponent<Item>().Nome;
+                Sprite imgObj = colidiu.gameObject.GetComponent<Item>().imagemObj;
+                Inventario.ReceberItem(nomeObj, imgObj);
+
+                Destroy(colidiu.gameObject);
+                podePegar = false;
+            }
+
+        }
+    }
+
+    public void AumentarHP(float vidaPlus)
+    {
+        hp += vidaPlus;
     }
 
 }
